64b94df6f74217e141afffc08bfc15880d9dc1b3
Zechariah <zechariahadebayo42@gmail.com>
2024-12-26 16:05:19 +0100

    chore: configured static root

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ require("./configs/db")//connect to db
 const express = require("express")
 const dbConnection = require("./configs/db")
 const baseRoutes = require("./routes/v1/index")
+const path = require("path")
 
 const app = express()
 // configure necessary middlewares
 app.use(express.json())
+app.use(express.static(
+    path.join(__dirname, "static")
+))
 app.use(baseRoutes)
 app.get("*", (req, res, next)=> res.send("Invalid route!"))
 
